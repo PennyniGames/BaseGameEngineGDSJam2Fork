f16360affaecebba8c199d2e3313f42f99bcf13b
= <nygell.saunders@hotmail.com>
2025-09-16 10:44:02 +1200

    Added an enum class for identifying the type of Actor.

---
+++ EActorType.h
@@ -0,0 +1,22 @@
+//
+// Created by penny on 16/09/25.
+//
+
+#ifndef EACTORTYPE_H
+#define EACTORTYPE_H
+
+enum class EActorType
+{
+    ShouldntExist,
+    Player,
+    Wall,
+    Enemy,
+    Boss,
+    Key,
+    Door,
+    Projectile,
+    Item,
+    Teleporter
+};
+
+#endif //EACTORTYPE_H
